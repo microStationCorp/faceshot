716389ef37120a03681468b0819ab52fe1d10e26
microStationCorp <sujanmondal916@gmail.com>
2020-03-06 16:21:12 +0530

    count model changed initial

--- addphoto/models.py
+++ addphoto/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django_resized import ResizedImageField
 import os
 from django.utils import timezone
+
+
 # Create your models here.
 
 
@@ -14,7 +16,7 @@ class UploadedPhoto(models.Model):
     dateOfPost = models.DateTimeField(default=timezone.now)
     uploader = models.ForeignKey(User, on_delete=models.CASCADE)
     caption = models.CharField(max_length=140, blank=True, null=True)
-    image = ResizedImageField(size=[500,500],upload_to=get_image_path)
-    fire_count= models.IntegerField(default=0)
-    poop_count= models.IntegerField(default=0)
-    haha_count= models.IntegerField(default=0)
+    image = ResizedImageField(size=[500, 500], upload_to=get_image_path)
+    fire_count = models.ManyToManyField(User, related_name="fire_count", blank=True)
+    poop_count = models.ManyToManyField(User, related_name="poop_count", blank=True)
+    haha_count = models.ManyToManyField(User, related_name="haha_count", blank=True)

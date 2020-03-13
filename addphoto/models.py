1706e8809676f467b88e9d4dc04631dd98b0ed90
microStationCorp <sujanmondal916@gmail.com>
2020-03-13 13:28:21 +0530

    newly database start

--- addphoto/models.py
+++ addphoto/models.py
@@ -7,6 +7,11 @@ from django.utils import timezone
 
 # Create your models here.
 
+class Seasons(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
 
 def get_image_path(instance, filename):
     return os.path.join('photos', str(instance.uploader.id), filename)
@@ -15,11 +20,17 @@ def get_image_path(instance, filename):
 class UploadedPhoto(models.Model):
     dateOfPost = models.DateTimeField(default=timezone.now)
     uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+    season=models.ForeignKey(Seasons,on_delete=models.CASCADE)
     caption = models.CharField(max_length=140, blank=True, null=True)
     image = ResizedImageField(size=[500, 500], upload_to=get_image_path)
-    haha_count = models.ManyToManyField(User, related_name="haha_count", blank=True)
-    fire_count = models.ManyToManyField(User, related_name="fire_count", blank=True)
-    poop_count = models.ManyToManyField(User, related_name="poop_count", blank=True)
+    haha_count = models.ManyToManyField(
+        User, related_name="haha_count", blank=True)
+    fire_count = models.ManyToManyField(
+        User, related_name="fire_count", blank=True)
+    poop_count = models.ManyToManyField(
+        User, related_name="poop_count", blank=True)
     hahaStat = models.BigIntegerField(default=0)
     fireStat = models.BigIntegerField(default=0)
     poopStat = models.BigIntegerField(default=0)
+
+
